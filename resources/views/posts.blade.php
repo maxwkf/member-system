51b641a57237e800b128108d3394e31ed0aa6452
Max Wong <maxwkf@gmail.com>
2022-05-18 11:44:51 +0000

    blade components and css grids

--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -1,4 +1,22 @@
 <x-layout>
+    @include('_posts-header')
+    <main class="max-w-6xl mx-auto mt-6 lg:mt-20 space-y-6">
+        @if ($posts->count() > 0)
+            <x-post-featured-card :post="$posts[0]" />
+
+            @if ($posts->count() > 1)
+            
+                <div class="lg:grid lg:grid-cols-6">
+                    @foreach($posts->skip(1) as $post)
+                        <x-post-card :post="$post" class="{{ $loop->iteration < 3 ? 'col-span-3' : 'col-span-2' }}" />
+                    @endforeach
+                </div>
+            @endif
+        @endif
+    </main>
+</x-layout>
+
+<!-- <x-layout>
     <x-slot name="content">
         @foreach ($posts as $post)
         <h1><a href="/posts/{{ $post->slug }}">{{ $post->title }}</a></h1>
@@ -12,4 +30,4 @@
         @endforeach
         
     </x-slot>
-</x-layout>
+</x-layout> -->
