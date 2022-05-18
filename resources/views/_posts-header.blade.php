864d952d93006a211154dec41a283326a34caed2
Max Wong <maxwkf@gmail.com>
2022-05-18 15:33:06 +0000

    how to extract dropdown blade

--- resources/views/_posts-header.blade.php
+++ resources/views/_posts-header.blade.php
@@ -15,30 +15,42 @@
         <!--  Category -->
         <div class="relative lg:inline-flex bg-gray-100 rounded-xl">
 
-            <div x-data="{ show: false }">
-                <button
-                    @click="show = ! show"
-                    class="py-2 pl-3 pr-9 text-sm font-semibold w-full lg:w-32 text-left flex lg:inline-flex"
-                >
-                    {{ isset($currentCategory) ? $currentCategory->name : 'Categories' }}
-                    <svg class="transform -rotate-90 absolute pointer-events-none" style="right: 12px;" width="22"
-                            height="22" viewBox="0 0 22 22">
-                        <g fill="none" fill-rule="evenodd">
-                            <path stroke="#000" stroke-opacity=".012" stroke-width=".5" d="M21 1v20.16H.84V1z">
-                            </path>
-                            <path fill="#222"
-                                    d="M13.854 7.224l-3.847 3.856 3.847 3.856-1.184 1.184-5.04-5.04 5.04-5.04z"></path>
-                        </g>
-                    </svg>
-                </button>
-                <div x-show="show" class="py-2 absolute bg-gray-100 rounded-xl mt-2 w-full z-50" style="display: none">
-                    <a href="/" class="block text-left px-3 text-sm leading-6 hover:bg-blue-500 focus:bg-blue-500 hover:text-white focus:text-white">All</a>
-                    @foreach ($categories as $category)
-                        
-                        <a href="{{ $category->slug }}" class="block text-left px-3 text-sm leading-6 hover:bg-blue-500 focus:bg-blue-500 hover:text-white focus:text-white">{{ $category->name }}</a>
-                    @endforeach
-                </div>
-            </div>
+            <x-dropdown>
+                <x-slot name="trigger">
+                    <button
+                        class="py-2 pl-3 pr-9 text-sm font-semibold w-full lg:w-32 text-left flex lg:inline-flex"
+                    >
+                        {{ isset($currentCategory) ? ucwords($currentCategory->name) : 'Categories' }}
+
+                        <x-icon name="down-arrow" />
+                    </button>
+                </x-slot>
+
+                {{-- 
+                   - Using routeIs need to set alias in web.php for corresponding route
+                   - 
+                --}}
+                <x-dropdown-item href="/" :active="request()->routeIs('home')">All</x-dropdown-item>
+
+                @foreach ($categories as $category)
+                
+                    {{-- 
+                       - Active can be check by the followings
+                       - 
+                       - 1. Check by variable
+                       -    :active="isset($currentCatetory) && $currentCategory->is($category)"    
+                       -
+                       - 2. Check by URI
+                       -    :active="request()->is('categories/' . $category->slug)"
+                    --}}
+                    <x-dropdown-item
+                        href="/categories/{{ $category->slug }}"
+                        :active='request()->is("categories/{$category->slug}")'
+                    >{{ ucwords($category->name) }}
+                    </x-dropdown-item>
+
+                @endforeach
+            </x-dropdown>
 
         </div>
 
--- routes/web.php
+++ routes/web.php
@@ -39,10 +39,10 @@ Route::get('/', function () {
     // });
     
     return view('posts', [
-        'posts' => Post::with('category')->with('author')->get(),
+        'posts' => Post::latest()->with('category')->with('author')->get(),
         'categories' => Category::all()
     ]);
-});
+})->name('home');
 
 Route::get('posts/{post:slug}', function (Post $post) {
 
@@ -56,7 +56,7 @@ Route::get('categories/{category:slug}', function (Category $category) {
         'currentCategory' => $category,
         'categories' => Category::all()
     ]);
-});
+})->name('category');
 
 Route::get('authors/{user}', function (User $user) {
 
